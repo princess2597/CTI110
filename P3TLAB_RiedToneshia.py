e1f5feb2eb6eb24be8dabebc8910a258b8bdf69a
princess2597 <36427271+princess2597@users.noreply.github.com>
2018-02-24 15:45:01 -0500

    Add files via upload

---
+++ P3TLAB_RiedToneshia.py
@@ -0,0 +1,20 @@
+#CTI -110
+#P3TLAB
+#Toneshia Ried
+#February 24, 2018
+
+
+Score = int( input("Please Enter Your Score"))
+
+if Score > 0 and Score <= 59:
+    print ("Your grade is F")
+elif Score > 59 and Score <= 69:
+    print ("Your grade is D")
+elif Score > 69 and Score <= 79:
+    print ("Your grade is C")
+elif Score > 79 and Score <= 89:
+    print ("Your grade is B")
+elif Score > 89 and Score <= 100:
+    print ("Your grade is A")
+
+

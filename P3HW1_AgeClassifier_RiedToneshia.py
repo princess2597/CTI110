f4fc7e9f718c501766844869906aef4dee3bceb0
princess2597 <36427271+princess2597@users.noreply.github.com>
2018-02-24 13:35:50 -0500

    Add files via upload

---
+++ P3HW1_AgeClassifier_RiedToneshia.py
@@ -0,0 +1,15 @@
+#CTI - 110
+#P3HW1 - Age Classifier
+#Toneshia Ried
+#February 24, 2018
+
+YourAge = int( input("Please enter your age:"))
+if YourAge <= 1:
+    print("You are an infant")
+elif YourAge < 13:
+    print ("You are a child")
+elif YourAge < 20:
+    print ("You are a teenager")
+elif YourAge >= 20:
+    print ("You are an adult")
+        

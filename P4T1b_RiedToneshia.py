2ab2e3e407060bad7313a87719dce73683220bb2
princess2597 <36427271+princess2597@users.noreply.github.com>
2018-03-18 12:30:11 -0400

    Add files via upload

---
+++ P4T1b_RiedToneshia.py
@@ -0,0 +1,38 @@
+#CTI - 110
+#P4T1b: Initials 
+#Toneshia Ried
+#March 18, 2018
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("yellow")
+
+pen1 = turtle.Turtle()
+pen1.color = ("Purple")
+pen1.pensize(10)
+pen1.forward(100)
+pen1.penup()
+pen1.forward(-50)
+pen1.pendown()
+pen1.left(-90)
+pen1.forward(100)
+
+pen1.penup()
+pen1.forward(-90)
+pen1.left(90)
+pen1.forward(150)
+pen1.pendown()
+pen1.right(90)
+pen1.forward(100)
+pen1.penup()
+pen1.forward(-100)
+pen1.pendown()
+pen1.left(90)
+pen1.forward(50)
+pen1.right(90)
+pen1.forward(50)
+pen1.left(-90)
+pen1.forward(50)
+pen1.left(140)
+pen1.forward(70)
+
---
+++ m5t1_RiedToneshia.py.py
@@ -0,0 +1,36 @@
+#CTI - 110
+#P4T1a: Shapes
+#Toneshia Ried
+#March 18, 2018
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("pink")
+wn.title("Tess & Alex")
+
+tess = turtle.Turtle()
+tess.color("purple")
+tess.pensize(5)
+
+alex = turtle.Turtle()
+alex.pensize(5)
+
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+
+
+
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+          
+wn.mainloop()
+
